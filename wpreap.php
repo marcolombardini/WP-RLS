dea092ea7674badc1b23df44944ff68e65be4e12
marcolombardini <marco.lombardini@gmail.com>
2015-02-27 10:59:21 -0500

    Fixed and Cleaned Code

--- wpreap.php
+++ wpreap.php
@@ -1,4 +1,5 @@
-/**
+<?php
+/*
  * Plugin Name: WPREAP (WordPress Real Estate Aggregator Plugin)
  * Plugin URI: https://github.com/marcolombardini/wpreap
  * Description: This plugin is designed to create an XML File so real estate aggregators can syndicate listings from your WordPress powered website.
@@ -9,17 +10,14 @@
  * Version Date: 2/26/15
  */
 
-<?php
-
-
+function do_feed_zillow() {
+	load_template( ABSPATH . '/wp-content/plugins/wpreap/zillow-template.php' );
+}
 function do_feed_streeteasy() {
 	load_template( ABSPATH . '/wp-content/plugins/wpreap/streeteasy-template.php' );
 }
 
-function do_feed_zillow() {
-	load_template( ABSPATH . '/wp-content/plugins/wpreap/zillow-template.php' );
-}
 
-add_action( 'do_feed_streeteasy', 'do_feed_streeteasy’, 10, 1 );
 add_action( 'do_feed_zillow', 'do_feed_zillow', 10, 1 );
+add_action( 'do_feed_streeteasy', 'do_feed_streeteasy', 10, 1 );
 
